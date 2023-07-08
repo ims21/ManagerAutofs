39102b04d417e90678536e149f162c287c674e54
ims <ims21@users.sourceforge.net>
2023-07-08 07:19:31 +0200

    2.14 - fixed several problems on first using - and bug fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setup_translate
 
 pkg = 'Extensions.ManagerAutofs'
 setup(name='enigma2-plugin-extensions-managerautofs',
-	version='2.13',
+	version='2.14',
 	description='manage autofs files',
 	packages=[pkg],
 	package_dir={pkg: 'plugin'},
